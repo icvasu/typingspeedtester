f2345eb5ffaaf7f51cc827f77bd87e158bac1826
icvasu <vasugupta2744@gmail.com>
2024-12-19 19:14:41 +0530

    first commit

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ string firstScreen()
     char ch = 1;
     string name;
     spaces();
-    cout << "\t\t\t\t    WELCOME TO THE SALIK TYPING SPEED TEST\n";
+    cout << "\t\t\t\t    WELCOME TO THE  TYPING SPEED TEST\n";
     spaces();
     cout << "\t\t\t--------------------------------------------------------------------\n";
     cout << "\t\t\t|   Impress the programmers with your Typing speed " << ch << "               |\n";
@@ -51,7 +51,7 @@ void paragraph(string name)
 {
     cout << "\n\t\t\t\t\t\t\t\t   WELCOME " << name << "\n";
     spaces();
-    cout << "\t\t\t\t\t\t\t\tSALIK TYPING SPEED TEST\n";
+    cout << "\t\t\t\t\t\t\t\t TYPING SPEED TEST\n";
     spaces();
     cout << "Being human makes us susceptible to the onset of feelings. The role of these emotions varies. Some of them are useful while others may be harmful. The use of social media for self-expression has reached a point that it makes us feel we can say anything. This begins when we see people expressing anything and everything that come to mind. When we see everyone else voicing their likes and dislikes, their irritations and desires we tend to imitate what they do. And because many engage in this, we think that it is normal and healthy. However, when we get used to unbridled self-expression, we come to believe that all feelings are valid. We become convinced that in real life, we should also act on our emotions and our impulses. Using social media this way erodes our ability to regulate our actions and reactions. To illustrate, when something small irritates us we think that it's okay to feel this way. But isn't it better to foster one's patience and resilience instead of immediately complaining? Or when we develop an attraction to someone despite that person being in a relationship, and because social media has conditioned us that all feelings can be expressed, we tend to think that acting on this attraction is okay. Not all feelings deserve expression. We find ourselves creating our own problems when we let our present emotions control our actions.\n";
     spaces();
